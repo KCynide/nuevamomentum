264e6feb1086c23bfe1709348024853f5f7599bf
KCynide <80189507+KCynide@users.noreply.github.com>
2022-01-24 01:49:57 +0900

    almost home
    yeah

--- js/weather.js
+++ js/weather.js
@@ -5,8 +5,13 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     console.log("You Live in", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        console.log(data.name, data.weather[0].main);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+                const weather = document.querySelector("#weather span:first-child");
+                const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 function onGeoError() {
