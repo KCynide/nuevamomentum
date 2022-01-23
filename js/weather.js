d1d740936d386546c5fe3dfd77ffdc855aa5af1e
KCynide <80189507+KCynide@users.noreply.github.com>
2022-01-23 14:16:15 +0900

    make a weather, city name
    yeah

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,15 @@
-navigator.geolocation.getCurrentPosition()
+const API_KEY = '0b9541132be1e1a1d73c6436f31f02b4';
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You Live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data.name, data.weather[0].main);
+    });
+}
+function onGeoError() {
+    alert("Where are you??");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
